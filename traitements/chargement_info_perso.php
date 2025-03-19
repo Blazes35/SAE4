f7d47470257818cde5fa10937f8fa5d66b3ffe5d
Blazes35 <loridam2@gmail.com>
2025-03-19 09:47:57 +0100

    Fix SQL query in chargement_info_perso.php to use a hardcoded email for testing

--- traitements/chargement_info_perso.php
+++ traitements/chargement_info_perso.php
@@ -3,9 +3,9 @@ require_once "loadenv.php";
 loadEnv();
 $db = dbConnect();
 // Préparez la requête SQL en utilisant des requêtes préparées pour des raisons de sécurité
-$requete = 'SELECT * FROM UTILISATEUR WHERE UTILISATEUR.Mail_Uti=:mail';
+$requete = 'SELECT * FROM UTILISATEUR WHERE UTILISATEUR.Mail_Uti=etstesetset';
 $stmt = $db->prepare($requete);
-$stmt->bindParam("mail",$_SESSION['Mail_Uti']); // "s" indique que la valeur est une chaîne de caractères
+//$stmt->bindParam("mail",$_SESSION['Mail_Uti']); // "s" indique que la valeur est une chaîne de caractères
 $stmt->execute();
 $result = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
