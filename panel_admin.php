c4373c4771bd73a80b3dd6e9d095709d372e9cc4
Blazes35 <loridam2@gmail.com>
2025-03-20 10:04:19 +0100

    Comment out database connection in panel_admin.php for debugging purposes

--- panel_admin.php
+++ panel_admin.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <?php
-//    require "language.php";
+    require "language.php";
     require_once "loadenv.php";
     ?>
     <title><?php echo $htmlMarque; ?></title>
